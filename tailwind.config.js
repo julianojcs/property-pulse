b9037d727ced11b5490a5a88c8bd57fca68be692
Juliano Costa Silva <apfjuliano@gmail.com>
2024-09-24 22:18:36 -0300

    Config Tailwind.config.js theme file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
       },
+      gridTemplateColumns: {
+        '70/30': '70% 28%'
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
     },
-  },
-  plugins: [],
-};
+    plugins: []
+  }
+}
