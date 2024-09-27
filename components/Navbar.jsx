67cf2ccb89eaa65ff96dfdd03004930a80e2ecf3
Juliano Costa Silva <apfjuliano@gmail.com>
2024-09-27 00:28:12 -0300

    Implementing Clossing Profile Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -175,6 +175,9 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-0'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false)
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -184,6 +187,9 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false)
+                      }}
                     >
                       Saved Properties
                     </Link>
