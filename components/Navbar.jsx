342fdd4266a4b6e4d741a58434306cf8a0dd828b
Juliano Costa Silva <apfjuliano@gmail.com>
2024-09-26 23:01:20 -0300

    Implementing Sign Out

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -192,6 +192,10 @@ const Navbar = () => {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false)
+                        signOut()
+                      }}
                     >
                       Sign Out
                     </button>
